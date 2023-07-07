a9243156db906da1973232a290b73597245b5c35
roberiof <antonioroberiofilho8@gmail.com>
2023-07-07 15:59:50 -0300

    style: gather/create/edit more utils styles

--- src/styles/components/UtilsStyles.jsx
+++ src/styles/components/UtilsStyles.jsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components'
+import styled , {css} from 'styled-components'
 import { Link } from "react-router-dom";
 
 export const PrimaryBtn = styled.button`
-    padding: .5rem 2rem;
+    padding: .8rem 1.3rem;
     background-color: ${({theme}) => theme.colors.primary};
     color: #ffffff;
     font-size: 1rem;
@@ -20,6 +20,31 @@ export const PrimaryBtn = styled.button`
 
 export const SubmitBtn = styled(PrimaryBtn)`
   background-color: ${({theme , disabled_style}) => disabled_style === 'true' ? theme.colors.disabledButton : theme.colors.primary} !important;
+  ${
+      (props) => 
+      props.disabled_style === 'true' && css`
+        &:hover{
+          cursor: default;
+          opacity: 1;
+          box-shadow: None;
+        }
+      ` 
+  }
+`
+
+export const PrimaryInput = styled.input`
+    width:100%;
+    padding: .8rem .5rem; 
+    background-color: ${({theme}) => theme.colors.input};
+    border: 0;
+    outline-width: 1px;
+    border-radius: 5px;
+    transition: 200ms all;
+    &:focus{
+      background-color: #f7d8d8;
+      box-shadow: 0px 0px 3px 0px #D80A30; 
+      transform: scale(1.01)
+    }
 `
 
 export const LinkStyle = styled(Link)`
@@ -41,5 +66,81 @@ export const ViewIcon = styled.span`
   color: ${({theme}) => theme.colors.lightText}; 
   *{
     pointer-events: none ;
+  }
+`
+
+export const BackPageIcon = styled.span`
+  cursor: pointer;
+  font-size: 1.5rem;
+  position: absolute;
+  top: .5rem;
+  left: 1rem ;
+  display: flex;
+  align-items: center;  
+  gap: .5rem;
+  &:hover{
+    transform: translateY(-4px);
+    ::after{
+        content:'Go back to ${({page}) => page} page';
+        font-size: .8rem;
+      }
+  }
+`
+
+export const WrapperContent = styled.div`
+  background-color: #ffffff;
+  max-width: 1200px;
+  width: 95%;
+  margin: 8rem auto;
+  padding: 2rem;
+  border-radius: 10px;
+  line-height: 2rem;
+  position: relative;
+  h1{
+    text-align: center;
+    padding-bottom: 3rem;
+    font-weight: 500;
+    color: ${({h1PrimaryColor, theme}) => { 
+      return h1PrimaryColor ? theme.colors.primaryDark : '#000'
+    }}
+  }
+  h2{
+    color: ${({theme}) => theme.colors.primary};
+    font-style: italic;
+    margin-bottom: 1rem 
+  }
+
+  h3{
+    text-decoration: underline;
+    margin-bottom: .4rem;
+  }  
+
+  p{
+    color: ${({theme, isParagraphLight}) => {
+      if(isParagraphLight)
+        return theme.colors.lightText
+      }
+    };
+  }
+
+  // strong texts inside "contact us" page
+  .strong-text{
+    font-weight: bold;
+    color: #000000;
+  }
+
+  .strong-email{
+    font-weight: bold;
+    color: ${({theme}) => theme.colors.primaryDark};
+  }
+
+  @media(max-width: ${({theme}) => theme.medias.sm}){
+    padding-inline: 1rem;
+    h1{
+      font-size: 1.8rem;
+    }
+    h2{
+      font-size: 1.2rem
+    }
   }
 `
