e220ffb413b347f64fbc559e473f2b5371577c5c
roberiof <antonioroberiofilho8@gmail.com>
2023-07-12 10:20:01 -0300

    refactor(login): create user context

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React from "react";
 
 import Header from './components/Header'
 import { Outlet } from "react-router-dom";
+
+// CONTEXT API 
+import { UserProvider } from './context/UserContext'
 import { ProductsProvider } from "./context/ProductsContext";
 
 export default function App(){
   return(
-    <ProductsProvider>
-      <Header/>
-      <Outlet/>
-    </ProductsProvider>
+    <UserProvider>
+      <ProductsProvider>
+        <Header/>
+        <Outlet/>
+      </ProductsProvider>
+    </UserProvider>
   )
 
 }
